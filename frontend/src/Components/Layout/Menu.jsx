1b136a1f982c34ffb40bccf27a883e7a8247b2c5
Samuel Santos <samuxlh@gmail.com>
2025-11-28 18:42:29 -0300

    Frontend Refactoring

--- frontend/src/Components/Layout/Menu.jsx
+++ frontend/src/Components/Layout/Menu.jsx
@@ -13,25 +13,13 @@ function Menu() {
                         <Link to="/login" className="nav-link">Login</Link>
                     </li>
                     <li className="nav-item">
-                        <Link to="/categorias" className="nav-link">Categoria Listar</Link>
+                        <Link to="/clientes" className="nav-link">Clientes</Link>
                     </li>
                     <li className="nav-item">
-                        <Link to="/nova-categoria" className="nav-link">Categoria Cadastrar</Link>
+                        <Link to="/quartos" className="nav-link">Quartos</Link>
                     </li>
                     <li className="nav-item">
-                        <Link to="/receitas-categoria" className="nav-link">Receita Listar por Categoria</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/receitas" className="nav-link">Receita Listar</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/nova-receita" className="nav-link">Receita Cadastrar</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/usuarios" className="nav-link">Usuario Listar</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/novo-usuario" className="nav-link">Usuario Cadastrar</Link>
+                        <Link to="/reservas" className="nav-link">Reservas</Link>
                     </li>
                 </ul>
             </div>
